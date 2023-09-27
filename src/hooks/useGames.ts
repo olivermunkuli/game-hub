cacc01d824aab63d884c9613e3d58f17f0166c42
Oliver Munkuli <olivermunkuli@outlook.com>
2023-09-27 19:08:02 +0200

    Building game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,12 +2,13 @@ import { useState, useEffect } from "react";
 import apiClient from '../services/api-client';
 import { CanceledError } from "axios";
 
-interface Game {
+export interface Game {
 	id: number;
 	name: string;
+    background_image: string;
 }
 
-interface FetchGameResponse {
+ interface FetchGameResponse {
 	count: number;
 	results: Game[];
 }
