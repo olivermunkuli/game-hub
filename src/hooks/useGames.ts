ce8eebb6ed42cc50b70e825e702f5abeba13329d
Oliver Munkuli <olivermunkuli@outlook.com>
2023-09-28 11:19:18 +0200

    Filter games by genre

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,5 @@
 import { useData } from "./useData";
+import { Genre } from "./useGenres";
 
 export interface Platform {
     id: number;
@@ -15,4 +16,4 @@ export interface Game {
 }
 
 
-export const useGames = () => useData<Game>("/games");
+export const useGames = (selectedGenre: Genre | null) => useData<Game>("/games", { params: { genres: selectedGenre?.id} }, [selectedGenre?.id]);
